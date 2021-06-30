83448f92fdf90c32916bf87714b30881f5931a18
Fumitoshi Morisato <fumitoshi.morisato@access-company.com>
2021-06-30 10:31:46 +0900

    Fix to use notebook
    because jupyterlab does not provide syntax highlighting of elixir code

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,4 +1,5 @@
-c.ServerApp.ip = '0.0.0.0'
-c.ServerApp.root_dir = './notebook'
-c.ServerApp.open_browser = False
-c.ServerApp.token = ''
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.root_dir = './notebook'
+c.NotebookApp.open_browser = False
+c.NotebookApp.token = ''
